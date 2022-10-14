5e35f80e0963813cee09597132fe921b13d9cd34
TRỌNG NHÂN <trongnhan@MacBook-Air-cua-TRONG.local>
2022-10-14 11:12:07 +0700

    Created a Main controller with home about contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'main/home'
+  get '/home', to: 'main#home'
+  get '/about', to: 'main#about'
+  get '/contact', to: 'main#ccontact'
   # Defines the root path route ("/")
   # root "articles#index"
 end
